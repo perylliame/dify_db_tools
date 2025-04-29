49d2fd3e8997f2367049dcd3b48b4aa6842ebdee
martsforever <martsforever@aliyun.com>
2025-04-29 12:58:02 +0800

    feat: remove code

--- sql_utils/DEMO_CONNECT_CONFIG.py
+++ sql_utils/DEMO_CONNECT_CONFIG.py
@@ -1,7 +1,7 @@
 DEMO_CONNECT_CONFIG = {
-    "host": '81.71.153.11',
-    "user": 'root',
-    "password": 'QQqqmariadb123456',
-    "database": 'plain',
-    "port": 14149
+    "host": '******',
+    "user": '******',
+    "password": '******',
+    "database": '******',
+    "port": 3306
 }
